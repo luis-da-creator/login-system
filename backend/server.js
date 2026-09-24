35c050485bcd4eb295e15a013c75f656595e8846
LuisDaCreator <luispronto55@gmail.com>
2026-09-25 00:58:28 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,7 @@ require("dotenv").config();
 
 const app = express();
 
-   app.use(cors({
-     origin: process.env.FRONTEND_URL
-   }));
+   app.use(cors());
 app.use(express.json());
 
 const userSchema = new mongoose.Schema({
