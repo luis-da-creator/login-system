cb1dd210ac9d56747d4ef195db3275676b00e0b4
LuisDaCreator <luispronto55@gmail.com>
2026-09-24 23:45:38 +0800

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL ="https://login-system-w7u3.onrender.com";;
+const API_URL ="https://login-system-w7u3.onrender.com";
 
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
