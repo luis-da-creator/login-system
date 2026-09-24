cfec9136623b9285182d32c1de60327d2ee5f1ad
luis-da-creator <luispronto55@gmail.com>
2026-09-24 21:37:00 +0800

    Update API URL for production

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "[https://login-system-w7u3.onrender.com](https://login-system-w7u3.onrender.com)";
 
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
